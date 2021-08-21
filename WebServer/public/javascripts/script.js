dd0d2b004e1fa5a12ecc2aca7c2026b07780a914
Seonu Kim <44015944+Aromdami@users.noreply.github.com>
2021-08-22 05:19:24 +0900

    채팅방 기능 및 스크롤 업데이트
    3개의 채팅방을 분리, 같은 방 안에 있어야만 대화가 가능하도록 함.
    사용자 이름 노출은 아직 개발 중.
    
    채팅 전체 UI 및 사용된 폰트 등의 사이즈 및 단위를 수정하여 채팅에 스크롤을 적용함

--- WebServer/public/javascripts/script.js
+++ WebServer/public/javascripts/script.js
@@ -1,22 +1,36 @@
 const socket = io();
 const chat = document.getElementById('submitter');
 const chatLog = document.getElementById('chatlog');
+const testJoin = document.getElementById('testJoin');
+const userlist = document.getElementById('userlist');
+
+var room = 1;
 var usrName =  document.getElementById("username").innerText;
+var socketID = socket.id;
 
+/* 채팅 전송 기능*/
 
+//채팅 전송 및 로그에 기록
 chat.addEventListener('submit', function (e) {
     var message = document.getElementById('chat');
-    socket.emit('chat message', message.value, usrName);
+    socket.emit('chat message', message.value, usrName, room);
     chatLog.appendChild(msgLogging(message.value, true));
     message.value = '';
     message.focus();
     e.preventDefault();
 });
 
-socket.on('chat message', function(message){
-    chatLog.appendChild(msgLogging(message, false));
-});
+//상대방 채팅 메시지 서버에서 가져옴
+socket.on
+(
+    'chat message', 
+    function(message)
+    {
+        chatLog.appendChild(msgLogging(message, false));
+    }
+);
 
+//전송, 수신된 메시지 구분 후 출력
 const msgLogging = (message, isMine) => {
     var msgLog = document.createElement('div');
     var msgType = isMine ? "mine" : "others";
@@ -26,6 +40,7 @@ const msgLogging = (message, isMine) => {
     return msgLog;
 };
 
+/*이름 변경*/
 function changeNameTest()
 {
     var newName = prompt("새로운 이름을 입력해주세요");
@@ -34,4 +49,83 @@ function changeNameTest()
     else
         document.getElementById("username").innerText = newName;
     usrName = newName;
+
 }
+
+function signIn()
+{
+    var signIn = document.getElementsByClassName('form_Login');
+    var signUp = document.getElementsByClassName('form_Join');
+    
+}
+
+/* 테스트용 로그인*/
+$('#testJoin').click(function (e){
+    e.preventDefault();
+    socket.emit('access', usrName);
+    socket.emit('users');
+});
+
+/* 채팅방 접속 */
+
+//채팅방에 접속, 접속된 채팅방 표시
+$('#chatRooms').on("click", "button", function(){
+    //다른 채팅 방을 선택할 시 room를 변경
+    if (room != $(this).data('id'))
+    {
+        room = $(this).data('id');
+        var newTitle = "채팅방 " + room + "번";
+    }
+    else
+        return;
+    //기존에 들어가있던 채팅방의 css 클래스에서 ON 상태를 제거함
+    $(this).parents().children().removeClass("ON");
+    //새로 들어갈 클래스에 ON을 추가함
+    $(this).addClass("ON");
+    //서버의 불도 들어오게 함
+    $(this).next().removeClass("OFF");
+    $(this).next().addClass("ON");
+    
+    //채팅방 제목을 변경
+    $('#chatTitle').html(newTitle);
+
+    //새로고침 용 이전 채팅방에서 사용했던 채팅을 비워줌
+    $('#chatlog').html("");
+    socket.emit('join chat', {room});
+});
+
+//채팅방에 들어와 있는 사용자 표시
+socket.on
+('users', 
+    function (data)
+    {   
+        let lst = "";
+        data.foreach(
+            (i) => {
+                if (i.socketID == socketID)
+                    lst += '<div class = "chUsers"> ${i.name} (나)</div>';
+                else
+                    lst += '<div class = "chUsers"> ${i.name}</div>';
+            });
+        $('#userlist').html(lst);
+    }
+);
+
+//채팅방 들어가고 나가기 표현
+/*
+socket.on
+('enter chat', 
+    function (data)
+    {
+        
+    }
+);
+
+socket.on
+('leave chat', 
+    function (data)
+    {
+        
+    }
+);
+*/
